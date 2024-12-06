d03b14f31bb2b0702a026350f5b82293abaefdc0
Pavlo890 <melnychuk.p_ak23@nuwm.edu.ua>
2024-12-06 21:41:59 +0200

    Feedback

--- Program.cs
+++ Program.cs
@@ -1,18 +1,35 @@
 ﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
-namespace LabWork
+namespace lab8
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+            string text = "Хрещення Русі було в 988 році.";
+            Console.WriteLine("Текст:");
+            Console.WriteLine(text);
+
+            string pattern = @"\b\d+\b";
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(text);
+
+            if (matches.Count > 0)
+            {
+                Console.WriteLine("\nВ тексті є числа:");
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nВ тексті немає чисел.");
+            }
         }
     }
 }
